39627464707f4cb41e2e0259ad9b61e72878b8a9
aminawk <68608505+aminawk@users.noreply.github.com>
2026-07-06 19:58:09 +0200

    Update and rename find-even-zipcodes to find-even-zipcodes.m

---
+++ SQL/find-even-zipcodes.m
@@ -0,0 +1,7 @@
+/*
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID, 2) = 0; 
+*/
+
+--looks at if a number divisible by 2 gives a remainder, if so then odd otherwise even, can also be written as WHERE ID % 2 = 0;
